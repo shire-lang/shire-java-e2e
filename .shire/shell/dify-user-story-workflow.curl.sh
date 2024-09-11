66bafd064074f1a2fc76f7f7c325f708857dbfb5
Phodal Huang <h@phodal.com>
2024-09-11 09:28:33 +0800

    Remove story_list from inputs in epic story and user story workflow scripts

--- .shire/shell/dify-user-story-workflow.curl.sh
+++ .shire/shell/dify-user-story-workflow.curl.sh
@@ -2,7 +2,7 @@ curl -X POST 'https://api.dify.ai/v1/completion-messages' \
   --header "Authorization: Bearer ${singleStoryKey}" \
   --header 'Content-Type: application/json' \
   --data-raw '{
-      "inputs": {"feature": "Hello, world!", "story_list": "作为购物中心电影观众，我想要提前预订电影场次相关的食物，以便于节省购买食物的时间，更好地安排观影时间。", "story": "添加零食和饮料至购物车影院观众在购票时添加零食和饮料提前准备好观影期间的零食和饮料"},
+      "inputs": {"feature": "xxx", "story_list": "作为购物中心电影观众，我想要提前预订电影场次相关的食物，以便于节省购买食物的时间，更好地安排观影时间", "story": "xxxx"},
       "response_mode": "streaming",
       "user": "phodal"
   }'
